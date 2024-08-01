9e31efc7191a9e998377c477deb089f74788ac3e
Josh Brooks <bigjf10@aol.com>
2024-08-01 00:13:35 -0400

    Initial Commit

---
+++ Website Viewer/Website_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Website_ViewerApp.swift
+//  Website Viewer
+//
+//  Created by Joshua Brooks on 8/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Website_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
